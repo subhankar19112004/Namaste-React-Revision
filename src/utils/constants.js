5cb109107bd4b6a4c623b8ab8ee75995ca101427
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-06-22 05:49:42 +0530

    added the original API instead of mock data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const LOGO_URL =
   "https://thumbs.dreamstime.com/b/food-delivery-logo-food-delivery-logo-vector-template-157000359.jpg";
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/";
+export const MENU_API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9276773&lng=77.6212965&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
