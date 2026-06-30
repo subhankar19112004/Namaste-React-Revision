1f1ae4b6a58844222f4d33dac5777edb341d3bb8
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-06-30 19:37:18 +0530

    Made About page basic skeleton

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className="text-lg mt-4">
+        Welcome to our restaurant! We are passionate about serving delicious
+        food made with the freshest ingredients. Our team of talented chefs
+        creates a diverse menu that caters to all tastes and preferences.
+        Whether you're craving a hearty meal or a light snack, we have something
+        for everyone. Join us for an unforgettable dining experience!
+      </p>
+    </div>
+  );
+};
+
+export default About;
